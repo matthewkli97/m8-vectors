1bfe5948816cae563b51775153ee4842b9eab137
Matthew Li <matthewli@D-108-179-173-85.dhcp4.washington.edu>
2017-04-06 13:49:55 -0700

    bonus and double bonus complete

--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -27,6 +27,18 @@ MarbleGame <- function(color.guess) {
 
 # Bonus: Play the marble game until you win, keeping track of how many tries you take
 
+have.not.won <- TRUE
+tries <- 0
+while(have.not.won) {
+  tries <- tries + 1
+  guess <- sample(marbles,1)
+  if(MarbleGame(guess) == "You guessed correctly") {
+    have.not.won <- FALSE
+  }
+}
+
+print(tries)
+
 ## Double bonus(answer not provided): play the game 1000X (until you win) and track the average number of tries
 # Is it what you expected based on the probability
 
